d2382335abc1dc5289d4a1c0fc332455f962af20
Ajay Dwivedi <ajaydwivedi873.com>
2024-01-30 15:57:45 +0530

    update in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api/webhook",
+    "/api/webhooks(.*)",
     "/question/:id",
     "/tags",
     "/tags/:id",
@@ -11,7 +11,6 @@ export default authMiddleware({
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook"],
 });
 
 export const config = {

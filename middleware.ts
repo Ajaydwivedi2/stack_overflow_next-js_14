0e4df386182ba3f957501a6952bc8d5f90ec74f4
Ajay Dwivedi <ajaydwivedi873.com>
2024-01-30 15:25:40 +0530

    update in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,14 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/api/webhook",
-    "question/:id",
+    "/question/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+  ignoredRoutes: ["/api/webhook"],
 });
 
 export const config = {

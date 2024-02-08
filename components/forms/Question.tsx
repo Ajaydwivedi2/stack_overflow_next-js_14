ee5e4b984ab5427ef3eb01099e83720b5e8e1e44
Ajay Dwivedi <ajaydwivedi873.com>
2024-02-08 14:09:14 +0530

    add the dark version of code editior

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -20,6 +20,7 @@ import { Badge } from "../ui/badge";
 import Image from "next/image";
 import { createQuestion, editQuestion } from "@/lib/actions/question.action";
 import { usePathname, useRouter } from "next/navigation";
+import { useTheme } from "@/context/ThemeProvider";
 
 interface Props {
   type?: string;
@@ -28,6 +29,7 @@ interface Props {
 }
 
 function Question({ type, questionDetails, mongoUserId }: Props) {
+  const { mode } = useTheme();
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
@@ -177,6 +179,8 @@ function Question({ type, questionDetails, mongoUserId }: Props) {
                       "codesample | bold italic forecolor | alignleft aligncenter " +
                       "alignright alignjustify | bullist numlist ",
                     content_style: "body { font-family:Inter; font-size:16px }",
+                    skin: mode === "dark" ? "oxide-dark" : "oxide",
+                    content_css: mode === "dark" ? "dark" : "light",
                   }}
                 />
               </FormControl>
